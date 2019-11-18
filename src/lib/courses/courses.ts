98f028be3df9a01e9c9627c8eda895833483eeba
hemankur <hemankur@hotmail.com>
2019-11-18 16:52:08 -0500

    API to check for conflict

--- src/lib/courses/courses.ts
+++ src/lib/courses/courses.ts
@@ -153,6 +153,89 @@ app.post('/api/courses/register', (req, res) => {
 });
 
 /**
+ * POST request that takes sid and courseID and adds them to the registered table if a record doesn't already exist.
+ * Also updates the active capacity
+ */
+app.post('/api/courses/check/', (req, res) => {
+    let sql = 'select * from registered where sid = ?';
+    let params = [req.body.sid];
+    let dayTime = [];
+    let newDayTime: string;
+    let courses = [];
+
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            console.log(err);
+        }
+        if (rows) {
+            for (const row of rows) {
+                if (row.courseID === req.body.courseID) {
+                    res.json({
+                        error: 'Duplicate',
+                        message: 'Student already registered'
+                    });
+                    return;
+                }
+            }
+            let sql = 'select * from courses where courseID in (select courseID from registered where registered.sid = ?)';
+            let params = [req.body.sid];
+
+            db.all(sql, params, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    for (const row of rows) {
+                        dayTime.push({key: row.day + '' + row.time, course: row});
+                        courses.push(row);
+                    }
+                    let sql = 'select * from courses where courseID = ?';
+                    let params = [req.body.courseID];
+
+                    db.get(sql, params, (err, row) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            newDayTime = row.day + row.time;
+                            let conflict = false;
+                            let conflictCourse: string;
+                            for (const item of dayTime) {
+                                if (item.key === newDayTime) {
+                                    conflict = true;
+                                    conflictCourse = item;
+                                }
+                            }
+                            if (conflict) {
+                                res.json({
+                                    error: 'Conflict',
+                                    message: 'Time conflict',
+                                    data: conflictCourse
+                                });
+                                return;
+                            } else {
+                                sql = 'select * from courses where courseID = ?';
+                                params = [req.body.courseID];
+                                db.get(sql, params, (err, row) => {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        if (row.active >= row.capacity) {
+                                            res.json({
+                                                error: 'Full',
+                                                message: 'Capacity full. Please try later.'
+                                            });
+                                            return;
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+/**
  * Patch request that takes sid and courseID and deleted the entry from the registered table, if present.
  */
 app.patch('/api/courses/unregister', (req, res) => {
